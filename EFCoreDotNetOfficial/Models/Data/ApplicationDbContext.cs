6f81f1ff2902302bf2587281134cdf8ee347064c
Paras Panchal <paraspanchal5555@gmail.com>
2023-12-24 16:10:09 +0530

    Added Migration and created dbcontext 24-Dec-2023

--- EFCoreDotNetOfficial/Models/Data/ApplicationDbContext.cs
+++ EFCoreDotNetOfficial/Models/Data/ApplicationDbContext.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EFCoreDotNetOfficial.Models.Data
-{
-    public class ApplicationDbContext : DbContext
-    {
+namespace EFCoreDotNetOfficial.Models.Data;
 
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; } = null;
+    public DbSet<Order> Orders { get; set; } = null;
+    public DbSet<Product> Products { get; set; } = null;
+    public DbSet<OrderDetail> OrderDetails { get; set; } = null;
+
+    //Access Modifier
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //Verbatim String
+        optionsBuilder.UseSqlServer(@"Server=nexuslite-pc\sqlexpress;Database=Bulky;User Id=sa;Password=Predeator;Trusted_Connection=False;TrustServerCertificate=True;MultipleActiveResultSets=true");
     }
+
 }
